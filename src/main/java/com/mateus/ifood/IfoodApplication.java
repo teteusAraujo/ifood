ebe8aa3294c9202b3287aaa280774ad782ecff92
Mateus Araújo <mateusaraujo996@gmail.com>
2021-02-14 17:01:21 -0300

    Criação do Projeto da API do Ifood

---
+++ src/main/java/com/mateus/ifood/IfoodApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.ifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateus/ifood/IfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.ifood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

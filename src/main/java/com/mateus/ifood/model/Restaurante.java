f7979d9d5c0366f3d23c117d717786d999e85fe6
Luis Miguel Souza <68262792+luismiguwl@users.noreply.github.com>
2021-04-12 19:53:49 -0300

    Implementação do Lombok para fins de limpeza de classe

--- src/main/java/com/mateus/ifood/model/Restaurante.java
+++ src/main/java/com/mateus/ifood/model/Restaurante.java
@@ -8,6 +8,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 @Entity
 public class Restaurante {
 	@Id
@@ -19,12 +24,7 @@ public class Restaurante {
 	private Boolean aberto;
 	private Date dataCadastro;
 	private Date dataAtualização;
-	
-	public Restaurante() {
-		
-	}
 
-	
 	public Restaurante(Long id, String nome, BigDecimal taxaFrete, Boolean ativo, Boolean aberto, Date dataCadastro,
 			Date dataAtualização) {
 		this.id = id;
@@ -35,89 +35,5 @@ public class Restaurante {
 		this.dataCadastro = dataCadastro;
 		this.dataAtualização = dataAtualização;
 	}
-	
-	public Long getId() {
-		return id;
-	}
-	
-	public void setId(Long id) {
-		this.id = id;
-	}
 
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public BigDecimal getTaxaFrete() {
-		return taxaFrete;
-	}
-
-	public void setTaxaFrete(BigDecimal taxaFrete) {
-		this.taxaFrete = taxaFrete;
-	}
-
-	public Boolean getAtivo() {
-		return ativo;
-	}
-
-	public void setAtivo(Boolean ativo) {
-		this.ativo = ativo;
-	}
-
-	public Boolean getAberto() {
-		return aberto;
-	}
-
-	public void setAberto(Boolean aberto) {
-		this.aberto = aberto;
-	}
-
-	public Date getDataCadastro() {
-		return dataCadastro;
-	}
-
-	public void setDataCadastro(Date dataCadastro) {
-		this.dataCadastro = dataCadastro;
-	}
-
-	public Date getDataAtualização() {
-		return dataAtualização;
-	}
-
-	public void setDataAtualização(Date dataAtualização) {
-		this.dataAtualização = dataAtualização;
-	}
-
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Restaurante other = (Restaurante) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
-	
 }
